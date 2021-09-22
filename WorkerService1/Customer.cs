6efccb775afbe4c4015cbe006d7edd6383416a99
anyataa <anyatmr@gmail.com>
2021-09-22 09:21:06 +0700

    add parameter instard of insert query

--- WorkerService1/Customer.cs
+++ WorkerService1/Customer.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Configuration;
 using Oracle.ManagedDataAccess.Client;
 using Oracle.ManagedDataAccess.Types;
 using Microsoft.Data.SqlClient;
+using System.Data;
+
 
 namespace WorkerService1
 {
@@ -22,6 +24,16 @@ namespace WorkerService1
         OracleConnection con = new OracleConnection();
         SqlConnection scon = new SqlConnection();
         private Config config = new Config();
+        private int fetchPerSeconds = 300;
+        private string fromTable = "customer";
+        private string toTable = "bu_table";
+       
+
+        private int id;
+        private string f_name;
+        private string l_name;
+        private DateTime insert_time;
+        
         public Customer(ILogger<Worker> logger, IConfiguration configuration)
         {
             _configuration = configuration;
@@ -33,25 +45,48 @@ namespace WorkerService1
             con.ConnectionString = _configuration["ConnectionStrings:OracleDBConnection"];
             con.Open();
             scon.ConnectionString = _configuration["ConnectionStrings:SqlServerDBConnection"];
-            scon.Open();
+  
 
             string limitData = _configuration["DataConfig:LimitData"];
             OracleCommand cmd = con.CreateCommand();
             cmd.CommandText = $"select id, f_name, l_name, insert_time  from CUSTOMER fetch first {limitData} rows only";
             OracleDataReader reader = cmd.ExecuteReader();
-
-            while (reader.Read())
-            {
-                int id = reader.GetInt16(0);
-                string f_name = reader.GetString(1);
-                string l_name = reader.GetString(2);
-                DateTime insert_time = reader.GetDateTime(3);
-                SqlCommand insert = new SqlCommand($"insert into trans_all(created_ts,a,b,d) values ( '{insert_time}' ,'{f_name}', '{l_name}', {id})", scon);
-               insert.ExecuteNonQuery();
-                _logger.LogInformation("Number of Data fetched : " + id + " " + f_name + " "+ l_name + " "+ insert_time, DateTimeOffset.Now);
-                _logger.LogInformation("Worker running at: {time} ", DateTimeOffset.Now);
-                await Task.Delay(1000);
+            string InsertData = $"insert into {toTable}(BU,created_time,inserted_time,item_id, a,b) values ( @BU2, @createdTime2 , @insertedTime2 , @itemId2, @fName2, @lName2)";
+            using (SqlCommand insertCmd = new SqlCommand(InsertData, scon)) {
+                insertCmd.Parameters.Add("@BU2", SqlDbType.VarChar, 50);
+                insertCmd.Parameters.Add("@createdTime2", SqlDbType.DateTime);
+                insertCmd.Parameters.Add("@insertedTime2", SqlDbType.DateTime);
+                insertCmd.Parameters.Add("@itemId2", SqlDbType.BigInt);
+                insertCmd.Parameters.Add("@fName2", SqlDbType.VarChar, 100);
+                insertCmd.Parameters.Add("@lName2", SqlDbType.VarChar, 100);
+                scon.Open();
+                while (reader.Read())
+                {
+                    insertCmd.Parameters["@BU2"].Value = fromTable;
+                    insertCmd.Parameters["@createdTime2"].Value = reader.GetDateTime(3);
+                    insertCmd.Parameters["@insertedTime2"].Value = DateTime.Now;
+                    insertCmd.Parameters["@itemId2"].Value = reader.GetInt16(0);
+                    insertCmd.Parameters["@fName2"].Value = reader.GetString(1);
+                    insertCmd.Parameters["@lName2"].Value = reader.GetString(2);
+                    insertCmd.ExecuteNonQuery();
+                    _logger.LogInformation("Number of Data fetched : " + id + " " + f_name + " " + l_name + " " + insert_time, DateTime.Now);
+                    await Task.Delay(1000);
+                }
+                scon.Close();
             }
+
+            //while (reader.Read())
+            //{
+            //    id = reader.GetInt16(0);
+            //    f_name = reader.GetString(1);
+            //    l_name = reader.GetString(2);
+            //    insert_time = reader.GetDateTime(3);
+            //    SqlCommand insert = new SqlCommand($"insert into {toTable}(BU,created_time,inserted_time,a,b,d) values ( '{fromTable}','{insert_time}' , '{DateTime.Now}' , '{f_name}', '{l_name}', {id})", scon);
+            //   insert.ExecuteNonQuery();
+            //    _logger.LogInformation("Number of Data fetched : " + id + " " + f_name + " "+ l_name + " "+ insert_time, DateTimeOffset.Now);
+            //    _logger.LogInformation("Worker running at: {time} ", DateTimeOffset.Now);
+            //    await Task.Delay(1000);
+            //}
         }
        
     }
